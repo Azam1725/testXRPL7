34196a8911fc6556baabd8e0fedbea2e76cfe713
Azam1725 <moch._mahardika_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:03:22 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,6 @@
+public class tes {
+    public static void main(String[] args) {
+        System.out.println("Nama saya azam dari kelas X RPL 7 yang berasal dari indonesia");
+        String a = "1";
+    }
+}
